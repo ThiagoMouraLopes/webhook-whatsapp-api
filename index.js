e7d27432bb03611582563670ca31408d10a3dfc4
ThiagoMouraLopes <Thiagomouralopes123@gmail.com>
2023-12-07 22:14:30 -0300

    changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/whatsapp-webhook", async (req, res) => {
